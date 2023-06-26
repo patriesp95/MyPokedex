99fddca7d09fa672665377a9a4368150b559a838
Patricia Martinez Espert <patricia.martinez@edreamsodigeo.com>
2023-06-26 18:24:56 +0200

    Generate network manager

---
+++ PokemonList/Managers/NetworkManager.swift
@@ -0,0 +1,52 @@
+//
+//  NetworkManager.swift
+//  PokemonList
+//
+//  Created by patricia.martinez on 26/6/23.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    let baseURL = "https://pokeapi.co/api/v2/pokemon"
+    private init() {}
+    
+    func getPokemons(completed: @escaping (ApiGenericResponse?, String?) -> Void){
+        let endpoint = baseURL
+        
+        guard let url = URL(string: endpoint) else {
+            completed(nil, "Invalid request. Please try again.")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completed(nil, "Unable to complete your request. Please check your internet connection.")
+                return
+            }
+            
+            guard let response = response  as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(nil, "Invalid response from the server. Please try again.")
+                return
+            }
+                        
+            guard let data = data else {
+                completed(nil, "The data received from the server was invalidad. Please try again.")
+                return 
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let pokemons = try decoder.decode(ApiGenericResponse.self, from: data)
+                completed(pokemons,nil)
+                
+            } catch {
+                completed(nil, "The data received from the server was invalidad. Please try again.")
+            }
+        }
+        
+        task.resume()
+    }
+}
