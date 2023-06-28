a58f385032974bc376b99902774e75cd09621173
Patricia Martinez Espert <patricia.martinez@edreamsodigeo.com>
2023-06-28 11:47:22 +0200

    to be continued

--- PokemonList/Components/Main/CustomPokemonCell.swift
+++ PokemonList/Components/Main/CustomPokemonCell.swift
@@ -12,6 +12,10 @@ class CustomPokemonCell: UITableViewCell {
     
     static let identifier = "CustomPokemonCell"
     
+    let baseImageURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites"
+    let baseURL = "https://pokeapi.co/api/v2/"
+    let baseURLPokemon = "https://pokeapi.co/api/v2/pokemon/"
+    
     private let pokemonImageView: UIImageView = {
         let myImageView = UIImageView()
         myImageView.contentMode = .scaleAspectFit
@@ -21,7 +25,7 @@ class CustomPokemonCell: UITableViewCell {
     
     private let pokemonLabel: UILabel = {
         let myLabel = UILabel()
-        myLabel.textColor = .label
+        myLabel.textColor = .black
         myLabel.textAlignment = .left
         myLabel.font = .systemFont(ofSize: 24, weight: .medium)
         myLabel.text = "Error"
@@ -37,10 +41,28 @@ class CustomPokemonCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configure(with image: UIImage, and label: String){
-        self.pokemonImageView.image = image
+    func configure(with image: String, and label: String){
+        //self.pokemonImageView.image = image
         self.pokemonLabel.text = label
+        self.pokemonImageView.downloaded(from: getPokemonSpriteURL(pokeURL: image))
+
+        
     }
+    
+    private func getPokemonSpriteURL(pokeURL: String) -> String {
+        
+        var finalURL = String()
+        
+        let firstReplacement = String(pokeURL.dropFirst(Int(baseURL.count))) // pokemon/1/
+        let secondReplacement = String(pokeURL.dropFirst(Int(baseURLPokemon.count))).dropLast(1) //1
+            
+        if let range = firstReplacement.range(of:secondReplacement) {
+            finalURL = firstReplacement.replacingCharacters(in: range, with:"\(secondReplacement).png") // pokemon/1.png/
+        }
+        
+        return String("\(baseImageURL)/\(finalURL)".dropLast(1)) //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+        }
 
     private func setupUI(){
         self.contentView.addSubview(self.pokemonImageView)
@@ -49,21 +71,41 @@ class CustomPokemonCell: UITableViewCell {
         self.pokemonImageView.translatesAutoresizingMaskIntoConstraints = false
         self.pokemonLabel.translatesAutoresizingMaskIntoConstraints = false
         
-//        NSLayoutConstraint.activate([
-//            
-//            self.pokemonImageView.topAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.topAnchor),
-//            self.pokemonImageView.bottomAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.bottomAnchor),
-//            self.pokemonImageView.leadingAnchor.constraint(equalTo:self.contentView.layoutMarginsGuide.leadingAnchor),
-//            self.pokemonImageView.heightAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.heightAnchor, constant: 90),
-//            self.pokemonImageView.widthAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.widthAnchor, constant: 90),
-//            
-//            self.pokemonLabel.leadingAnchor.constraint(equalTo: self.pokemonImageView.trailingAnchor, constant: 16),
-//            self.pokemonLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -8),
-//            self.pokemonLabel.topAnchor.constraint(equalTo:self.contentView.topAnchor),
-//            self.pokemonLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
-//            
-//        ])
+        NSLayoutConstraint.activate([
+            
+            self.pokemonImageView.topAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.topAnchor),
+            self.pokemonImageView.bottomAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.bottomAnchor),
+            self.pokemonImageView.leadingAnchor.constraint(equalTo:self.contentView.layoutMarginsGuide.leadingAnchor),
+            self.pokemonImageView.heightAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.heightAnchor, constant: 90),
+            self.pokemonImageView.widthAnchor.constraint(equalTo: self.contentView.layoutMarginsGuide.widthAnchor, constant: 90),
+            
+            self.pokemonLabel.leadingAnchor.constraint(equalTo: self.pokemonImageView.trailingAnchor, constant: 16),
+            self.pokemonLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -8),
+            self.pokemonLabel.topAnchor.constraint(equalTo:self.contentView.topAnchor, constant: 50),
+            self.pokemonLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: 50)
+            
+        ])
         
     }
 }
 
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
--- PokemonList/Controllers/Main/MainViewController.swift
+++ PokemonList/Controllers/Main/MainViewController.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    let baseImageURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites"
-    let baseURL = "https://pokeapi.co/api/v2/"
-    let baseURLPokemon = "https://pokeapi.co/api/v2/pokemon/"
-    
     var pokemons = [ApiPokemonResponse]()
     var pokemonSprites = [UIImage]()
         
@@ -20,12 +16,17 @@ class MainViewController: UIViewController {
         let tableView = UITableView()
         tableView.backgroundColor = .white
         tableView.allowsSelection = true
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: CustomPokemonCell.identifier)
+        tableView.register(CustomPokemonCell.self, forCellReuseIdentifier: CustomPokemonCell.identifier)
         return tableView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        setupUI()
         
         NetworkManager.shared.getPokemons { pokemons, errorMessage in
             guard let pokemons = pokemons?.results else {
@@ -34,15 +35,11 @@ class MainViewController: UIViewController {
             }
             
             self.pokemons.append(contentsOf: pokemons)
+            
+            DispatchQueue.main.async(){
+                self.tableView.reloadData()
+            }
         }
-        
-       
-           
-        
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-        
-        setupUI()
     }
 
     private func setupUI(){
@@ -60,21 +57,6 @@ class MainViewController: UIViewController {
         ])
     }
     
-    private func getPokemonSpriteURL(pokeURL: String) -> String {
-        
-        var finalURL = String()
-        
-        let firstReplacement = String(pokeURL.dropFirst(Int(baseURL.count)))
-        let secondReplacement = String(pokeURL.dropFirst(Int(baseURLPokemon.count))).dropLast(1)
-            
-        if let range = firstReplacement.range(of:secondReplacement) {
-            finalURL = firstReplacement.replacingCharacters(in: range, with:"\(secondReplacement).png")
-        }
-        
-        return String("\(baseImageURL)/\(finalURL)".dropLast(1))
-
-    }
-    
 }
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
@@ -83,33 +65,13 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CustomPokemonCell.identifier, for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomPokemonCell.identifier, for: indexPath) as? CustomPokemonCell else {
+            fatalError("couldnt dequeue")
+        }
         
-        cell.textLabel?.text = pokemons[indexPath.row].name
-        cell.imageView?.downloaded(from: getPokemonSpriteURL(pokeURL: pokemons[indexPath.row].url))
-                
-        return cell
-    }
-  
-}
+        let item = pokemons[indexPath.row]
+        cell.configure(with: item.url, and: item.name)
 
-extension UIImageView {
-    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
-        contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
-            }
-        }.resume()
-    }
-    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
-        guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
+        return cell
     }
 }
